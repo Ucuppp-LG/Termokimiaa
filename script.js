e330429739fbf21a816947ef75df19aec6609aad
Ucuppp-LG <im429343@gmail.com>
2025-03-17 11:45:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+        Scrollreveal().reveal('.main-title, .section-title', {delay: 500, origin:'left'});
+        Scrollreveal().reveal('.sec-01, .image, .info', { delay: 600, origin:'bottom' });
+        Scrollreveal().reveal('.text.box', { delay: 700, origin:'right' });
+        Scrollreveal().reveal('.sec-02 .image, .sec-03 .image', { delay: 500, origin:'top' });
